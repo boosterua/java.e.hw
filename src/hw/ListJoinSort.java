7e9683ad68162f346e36f67a5d09d69715778454
tonchief <tonchief@gmail.com>
2016-12-11 15:56:57 +0200

    upd

--- src/hw/ListJoinSort.java
+++ src/hw/ListJoinSort.java
@@ -8,15 +8,15 @@ import java.util.*;
  * 14. Написать generic метод объединения двух списков и 
  * c последующей сортировкой полученного списка.
  */
-public class ListJoinSort<E extends Custom> {
+public class ListJoinSort<E extends Worker> {
+    private List<E> listJ;
 
-
-    List<E> list1 = new ArrayList<>();
-    List<E> list2 = new ArrayList<>();
-    List<E> listJ = listJoin(list1, list2);
-
-    public List listJoin(List list01, List list02){
-        List listJ = new ArrayList(list01);
+    public List<E> listJoin(List<E> list01, List<E> list02){
+        if (list01==null || list02==null || (list01.isEmpty() && list02.isEmpty())){
+            listJ = new ArrayList();
+            return listJ;
+        }
+        listJ = new ArrayList(list01);
         listJ.addAll(list02);
         sort(listJ);
         return listJ;
@@ -24,8 +24,11 @@ public class ListJoinSort<E extends Custom> {
         ArrayList<E> listJ = new ArrayList();
         for (E elem: list01) listJ.add(elem);
         for (E elem: list02) listJ.add(elem);
-        return listJ;
-    */
+        return listJ;    */
+    }
+
+    public List<E> getList(){
+        return this.listJ;
     }
 
     private List<E> sort(List<E> list){
@@ -41,9 +44,7 @@ public class ListJoinSort<E extends Custom> {
         int i = a;
         int j = b;
         E pivot = list.get(a + (b-a)/2);
-//        String pivotStr = pivot.toString();
         while (i<=j){
-//            while (list.get(i).toString().compareTo(pivotStr) < 0 )
             while (list.get(i).compareTo(pivot) < 0 )
                 i++;
             while (list.get(j).compareTo(pivot) > 0)
@@ -66,138 +67,67 @@ public class ListJoinSort<E extends Custom> {
     }
 
     public static void main(String[] args) {
-//        List<String> list1 = new ArrayList<>();
-//            list1.add(new Custom<String> ("A"));
-//            list1.add("D");
-//            list1.add("C");
-//
-//        List<String> list2 = new ArrayList<String>() {{
-//            add("F");
-//            add("B");
-//            add("E");
-//        }};
-//        Factory fac = new Factory();
 
-        List<String> list1 = new ArrayList<>();
-            list1.add(new Custom("A");
-            list1.add("D");
-            list1.add("C");
+        List<Worker> company1 = new ArrayList<>();
+            company1.add(new Worker("John", "Leager", 1234567890));
+            company1.add(new Worker("Greg", "Mouse", 1334567890));
+            company1.add(new Worker("Kevin", "Eaton", 1434567890));
 
-        List<String> list2 = new ArrayList<String>() {{
-            add("F");
-            add("B");
-            add("E");
-        }};
+        List<Worker> company2 = new ArrayList<>();
+            company1.add(new Worker("Gary", "Winter", 1934567890));
+            company1.add(new Worker("Ivan", "Kanicky", 1834567890));
+            company1.add(new Worker("Phillip", "Lenon", 1734567890));
 
-        List <Custom> list = new ListJoinSort().listJoin(list1, list2);
-//        list.li
+        List <Worker> list = new ListJoinSort().listJoin(company1, company2);
         System.out.println(list);
     }
 
 
 }
 
-class MyClass<E> {
-    private final Collection<? extends E> data;
-    MyClass(Comparator<E> comparator, Collection<? extends E> data) {
-        this.data = data;
-    }
-    public void processData() {
-        // ...
-    }
-}
-class Factory {
-    static <E extends Comparable<E>> MyClass<E> create(Collection<? extends E> data) {
-        return new MyClass<E>(new Comparator<E>() {
-            public int compare(E o1, E o2) {
-                return o1.compareTo(o2);
-            }
-        }, data);
-    }
-//    static <T> MyClass<T> create(Comparator<T> comparator, Collection<? extends T> data) {
-//        return new MyClass<T>(comparator, data);
-//    }
-}
+class Worker implements Comparable<Worker>{
+    private String firstName;
+    private String lastName;
+    private Long taxId;
 
-
-class Custom <E extends Comparable<E>> implements Comparable<Custom> {
-    private Collection<? extends E> data;
-    private E   e;
-
-    Custom (Object e){
-        this.e = (E)e;
+    public Worker(String firstName, String lastName, long taxId){
+        if (firstName == null || lastName == null || taxId<=0)
+            throw new NullPointerException();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.taxId = taxId;
     }
+
+    public String name(){
+        return this.firstName+" "+this.lastName;
+
+    }
+
+    public String toString(){
+        return ""+ this.taxId + ". "+ name() + "";
+    }
+
 
     @Override
-    public int compareTo(Custom custom) {
-        return e.compareTo((E)custom);
+    public int compareTo(Worker employee) {
+        return taxId.compareTo(employee.taxId);
     }
 }
 
 
+//class Custom <E extends Comparable<E>> implements Comparable<Custom> {
+//    private Collection<? extends E> data;
+//    private E   e;
 //
-//class Util <E> {
-//
-//    public static List listJoin(List list01, List list02){
-//        List listJ = new ArrayList(list01);
-//        listJ.addAll(list02);
-//        return listJ;
-//    /*  MANUAL METHOD
-//        ArrayList<E> listJ = new ArrayList();
-//        for (E elem: list01) listJ.add(elem);
-//        for (E elem: list02) listJ.add(elem);
-//        return listJ;
-//    */
-//    }
-//    public List<E> sort(List<E> list){
-//        if(list==null || list.isEmpty())
-//            return Collections.emptyList();
-//        int max = list.size();
-//        quickSort(0, max - 1, list);
-//        return list;
+//    Custom (Object e){
+//        this.e = (E)e;
 //    }
 //
-//
-//    private void quickSort(int a, int b, List<E> list){
-//        int i = a;
-//        int j = b;
-//        E pivot = list.get(a + (b-a)/2);
-//        String pivotStr = pivot.toString();
-//        while (i<=j){
-//            while (list.get(i).toString().compareTo(pivotStr) < 0 )
-//                i++;
-//            while (list.get(j).toString().compareTo(pivotStr) > 0)
-//                j--;
-//            if (i<=j) {
-//                // Swap:
-//                if (i!=j) {
-//                    E t = list.get(i);
-//                    list.set(i, list.get(j));
-//                    list.set(j, t);
-//                }
-//                i++;
-//                j--;
-//            }
-//        }
-//        if (a < j)
-//            quickSort(a, j, list);
-//        if (i < b)
-//            quickSort(i, b, list);
+//    @Override
+//    public int compareTo(Custom custom) {
+//        return e.compareTo((E)custom);
 //    }
-//
-//
-////    @Override
-////    public int compareTo(Object o) {
-////        return 0;
-////    }
 //}
-//
-
-
-
-
-
-
 
 
 
